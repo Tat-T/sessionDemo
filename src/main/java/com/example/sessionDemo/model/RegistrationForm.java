af171b831cd87a88fcd94bf3b10be7665904cb70
Tat-T <ring_87_87@mail.ru>
2025-04-30 19:46:17 +0300

    Navbar

---
+++ src/main/java/com/example/sessionDemo/model/RegistrationForm.java
@@ -0,0 +1,39 @@
+package com.example.sessionDemo.model;
+
+import com.example.sessionDemo.validation.PasswordMatches;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@PasswordMatches
+public class LoginForm {
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
